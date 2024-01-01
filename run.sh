e9065a468837ab02716e96d67f83cb2aa7b95ee6
dmitry.ishmitov <dmitry.ishmitov@reltio.com>
2024-01-01 08:14:47 +0000

    - fixed port

--- run.sh
+++ run.sh
@@ -3,8 +3,7 @@
 echo "Script execution started."
 source setenv.sh
 # Run the main Python script
-uvicorn src.controller:app
-
+uvicorn src.controller:app --port 8080
 # Deactivation is not strictly necessary for a script, as the environment changes
 # do not persist when the script finishes
 echo "Script execution finished."
