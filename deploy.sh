acc76225f675ce54061bba7fadb8b328b14f53cc
dmitry.ishmitov <dmitry.ishmitov@reltio.com>
2024-01-02 23:38:44 +0000

    - fixed deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
+set -e
 
 echo "$SSH_PRIVATE_KEY" > private_key.pem
-chmod 400 private_key.pem
+chmod 700 private_key.pem
 
-ssh -o "StrictHostKeyChecking=no" -i private_key.pem ec2-user@$EC2_HOST << 'ENDSSH'
+ssh -vvv -o "StrictHostKeyChecking=no" -i private_key.pem ec2-user@$EC2_HOST << 'ENDSSH'
   cd /home/ec2-user/image-gen
+  git reset --hard
   git pull
+  chmod +x run.sh
   ./run.sh
 ENDSSH
+
 
 rm -f private_key.pem
