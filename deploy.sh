78cc612c79186fbea62c780e19fd28f17579ecbd
dmitry.ishmitov <dmitry.ishmitov@reltio.com>
2024-01-03 00:24:35 +0000

    - fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 set -e
 
-echo $SSH_PRIVATE_KEY
-echo $EC2_HOST
+EC2_HOST=ec2-18-206-220-10.compute-1.amazonaws.com
 
 echo "$SSH_PRIVATE_KEY" > private_key.pem
 chmod 700 private_key.pem
 
-ssh -vvv -o "StrictHostKeyChecking=no" -i private_key.pem ec2-user@$EC2_HOST << 'ENDSSH'
+ssh -o "StrictHostKeyChecking=no" -i private_key.pem ec2-user@$EC2_HOST << 'ENDSSH'
+  PID=$(lsof -t -i :8000)
+  if [ ! -z "$PID" ]; then
+    kill -9 $PID
+  fi
+
   cd /home/ec2-user/image-gen
-  git reset --hard
   git pull
-  chmod +x run.sh
-  ./run.sh
+  nohup bash run.sh &
 ENDSSH
 
 
