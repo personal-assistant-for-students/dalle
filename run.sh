1b7bce2b75c97f013b02485b251cfef6d2fec171
Ishmitov Dmitrii <60426238+dmitr2ish@users.noreply.github.com>
2024-01-03 09:16:57 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ mkdir images
 source setenv.sh
 pytest
 # Run the main Python script
-uvicorn src.controller:app --host '0.0.0.0' --port 8000
+uvicorn src.controller:app --host '0.0.0.0' --port 8000 &
 # Deactivation is not strictly necessary for a script, as the environment changes
 # do not persist when the script finishes
 echo "Script execution finished."
