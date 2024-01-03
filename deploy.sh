c340afc2ce7c4366364061ee81c8713b4723e102
Ishmitov Dmitrii <60426238+dmitr2ish@users.noreply.github.com>
2024-01-03 01:00:36 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ set -e
 
 EC2_HOST=ec2-18-206-220-10.compute-1.amazonaws.com
 
-echo "$SSH_PRIVATE_KEY" > private_key.pem
+echo $SSH_PRIVATE_KEY > private_key.pem
 chmod 700 private_key.pem
 
 ssh -o "StrictHostKeyChecking=no" -i private_key.pem ec2-user@$EC2_HOST << 'ENDSSH'
